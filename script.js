bb9803849f54397641efd9d6f5a48656efaf0210
SANTHOSH P <santhosh11042007@gmail.com>
2026-07-30 22:12:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-async function loadMessage() {
-    const res = await fetch("http://127.0.0.1:5000/api/message");
-    const data = await res.json();
-    document.getElementById("result").innerText = data.message;
-}
